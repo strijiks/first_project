1d77ecf3074286ff99f97734d3d31074ed0e7c7e
Anastasia Olkhovskaia <strijik@yandex.ru>
2026-02-01 02:35:36 +0300

    Добавить .env в ignore

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
 print('Hello from online!')
+
+
+# Импорт load_dotenv.
+from dotenv import load_dotenv 
+
+# Импорт библиотеки для работы с окружением.
+import os  
+
+# Загрузка переменных из .env
+load_dotenv()
+
+def print_author():
+# Теперь переменные доступны через os.environ
+	author  = os.getenv('AUTHOR')
+	print(f"Автор проекта: {author}")
+print_author()
